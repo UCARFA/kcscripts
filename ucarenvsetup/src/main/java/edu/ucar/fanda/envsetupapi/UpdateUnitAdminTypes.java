c351564be36b6652207c8720021d211b1e1d4556
Steve Waltman <stevew@ucar.edu>
2017-07-31 14:08:06 -0600

    Modifications to UpdateUnitAdminTypes.java and supporting files

--- ucarenvsetup/src/main/java/edu/ucar/fanda/envsetupapi/UpdateUnitAdminTypes.java
+++ ucarenvsetup/src/main/java/edu/ucar/fanda/envsetupapi/UpdateUnitAdminTypes.java
@@ -2,13 +2,14 @@ package edu.ucar.fanda.envsetupapi;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
+import com.opencsv.CSVReader;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -23,8 +24,10 @@ public class UpdateUnitAdminTypes {
 	private static String UNIT_ADMINISTRATOR_API_URL;
 	private static String REST_API_USER;
 	private static String REST_API_PASSWORD;
+	private static String server;
 	
 	public UpdateUnitAdminTypes (String serverName) {
+		server = serverName;
 		UNIT_ADMIN_TYPE_API_URL = "https://" + serverName + ".fanda.ucar.edu/ra/research-common/api/v1/unit-administrator-types/";
 		UNIT_ADMINISTRATOR_API_URL = "https://" + serverName + ".fanda.ucar.edu/ra/research-common/api/v1/unit-administrators/";
 		REST_API_USER = "apiuser";
@@ -32,10 +35,18 @@ public class UpdateUnitAdminTypes {
 	}
 	
 	public static void main(String[] args) {
-		try {				
-			unitAdminTypeDelete();	
+		try {
+			System.out.println("\n   ***** Running UpdateUnitAdminTypes(); *****\n");
+			server = "localhost";
+			UNIT_ADMIN_TYPE_API_URL = "http://localhost:8080/kc-dev/research-common/api/v1/unit-administrator-types/";
+			UNIT_ADMINISTRATOR_API_URL = "http://localhost:8080/kc-dev/research-common/api/v1/unit-administrators/";
+			REST_API_USER = "admin";
+			REST_API_PASSWORD = "restapipassword";
+//			unitAdminTypeDelete();	
 			unitAdminTypeInsert();
+			deleteProjectAccountants();
 			assignProjectAccountants();
+			System.out.println("\n   ***** End UpdateUnitAdminTypes(); *****\n");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -54,26 +65,45 @@ public class UpdateUnitAdminTypes {
 	}
 	
 	public static String getKualiUserId(String userName) throws ClassNotFoundException {
-		System.out.println("   ********************** getKualiUsers() ***********************\n");
+	//	System.out.println("   ********************** getKualiUsers() ***********************\n");
 		String userId = null;
 		try {
-			userId = DbUtils.getUserId(userName);
+			userId = DbUtils.getUserId(userName, server);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("   ********************* END getKualiUsers() ********************\n");
+	//	System.out.println("   ********************* END getKualiUsers() ********************\n");
 		return userId;
 	}
 	
-	public static void assignProjectAccountants() throws IOException {
-		System.out.println("***************** assignProjectAccountants() *****************\n");
-		InputStream inputStream = UpdateUnitAdminTypes.class.getClassLoader().getResourceAsStream("unit_admin_projacct_assignment.json");
-		String jsonString = IOUtils.toString(inputStream, Charset.defaultCharset());
-		JSONArray jsonFileArray = new JSONArray(jsonString);
+	public static void deleteProjectAccountants() throws IOException {
+		System.out.println("***************** deleteProjectAccountants() *****************");
+		
+		InputStream inputStream = UpdateUnitAdminTypes.class.getClassLoader().getResourceAsStream("unit_admin_projacct_delete.csv");
+		CSVReader reader = null;		
+		JSONArray jsonFileArray = new JSONArray();
+		try {
+			reader = new CSVReader(new InputStreamReader(inputStream));
+			String[] line;
+            while ((line = reader.readNext()) != null) {
+            	JSONObject jsonObject = new JSONObject();
+            	jsonObject.put("userName", "" + line[0] + "");
+            	jsonObject.put("adminAcctTypeName", "" + line[1] + "");
+            	jsonObject.put("unitNumber", "" + line[2] + "");  			
+    			jsonFileArray.put(jsonObject);
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 		inputStream.close();
 		
+		Client client = Client.create();
+		// add basic authentication
+		client.addFilter(new HTTPBasicAuthFilter(REST_API_USER, REST_API_PASSWORD));
+		
 		try {
-			JSONArray jsonUpdateArray = new JSONArray();
+			String existingUnitAdminTypes = getExistingUnitAdminTypes();
+			JSONArray unitAdminTypeJsonArray = new JSONArray(existingUnitAdminTypes);
 			for (int i = 0; i < jsonFileArray.length(); ++i) {
 				JSONObject jsonObj = jsonFileArray.getJSONObject(i);
 				String userId = getKualiUserId(jsonObj.getString("userName"));
@@ -81,105 +111,243 @@ public class UpdateUnitAdminTypes {
 				if (userId == null) {
 					System.out.println("  ** User " + jsonObj.getString("userName") + " not found, or multiple records were returned for that user. **");
 				} else {
-					System.out.println("User ID for user " + jsonObj.getString("userName") + " is " + userId);
-			
-					String existingUnitAdminTypes = getExistingUnitAdminTypes();
-					
 					String adminTypeCode = null;
 					boolean found = false;
-					if (!existingUnitAdminTypes.equals(null)) {
-						JSONArray unitAdminTypeJsonArray = new JSONArray(existingUnitAdminTypes);
-						
+					if (!existingUnitAdminTypes.equals(null)) {			
 						for (int ii = 0; ii < unitAdminTypeJsonArray.length(); ++ii) {
 							JSONObject unitAdminType = unitAdminTypeJsonArray.getJSONObject(ii);
-							if (unitAdminType.getString("description").equals(jsonObj.getString("adminAcctTypeName")) && unitAdminType.getString("defaultGroupFlag").equals(jsonObj.getString("adminAcctType"))) {
-								System.out.println("Type code for " + jsonObj.getString("adminAcctTypeName") + " is " + unitAdminType.getString("code"));
+							if (unitAdminType.getString("description").equals(jsonObj.getString("adminAcctTypeName"))) {
+				//				System.out.println("Type code for " + jsonObj.getString("adminAcctTypeName") + " is " + unitAdminType.getString("code"));
 								adminTypeCode = unitAdminType.getString("code");
 								found = true;
 								break;
 							}
 						}
-						
 					}
 					if (!found) {
 						System.out.println("   *** Admin Type Code for " + jsonObj.getString("adminAcctTypeName") + " not found. ***");
 					} else {
-						System.out.println("User id: " + userId + ", Admin Type Code: " + adminTypeCode + ", Unit Number: " + unitNumber);		
+					//	System.out.println("User id: " + userId + ", Admin Type Code: " + adminTypeCode + ", Unit Number: " + unitNumber);		
+						String unitAdminKey = userId + ":" + adminTypeCode + ":" + unitNumber;
+						WebResource webResource = client.resource(UNIT_ADMINISTRATOR_API_URL + unitAdminKey);
+						ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
 						
-						JSONObject updateObj = new JSONObject();
-						updateObj.put("personId", "" + userId + "");
-						updateObj.put("unitAdministratorTypeCode", "" + adminTypeCode + "");
-						updateObj.put("unitNumber", "" + unitNumber + "");
-						System.out.println("Update Object: " + updateObj);
+						if (response.getStatus() == 200) {
+							ClientResponse delResponse = webResource.type("application/json").delete(ClientResponse.class);
+							if (delResponse.getStatus() != 204) {
+								System.out.println("ERROR - there was a problem deleting the record for key: " + unitAdminKey);
+							} else {
+								System.out.println("   Successful deletion of unit administrator: " + (jsonObj.getString("userName")) + ", " + (jsonObj.getString("adminAcctTypeName")) + ", " + unitNumber);  
+							}
+						} else {
+							System.out.println("   Unit administrator to delete does NOT exist in Kuali: " + (jsonObj.getString("userName")) + ", " + (jsonObj.getString("adminAcctTypeName")) + ", " + unitNumber);
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.destroy();
+		}	
+		System.out.println("*************** END deleteProjectAccountants() ***************\n");
+	}
+	
+	public static void assignProjectAccountants() throws IOException {
+		System.out.println("***************** assignProjectAccountants() *****************");
+	
+		InputStream inputStream = UpdateUnitAdminTypes.class.getClassLoader().getResourceAsStream("unit_admin_projacct_assignment.csv");
+		CSVReader reader = null;		
+		JSONArray jsonFileArray = new JSONArray();
+		try {
+			reader = new CSVReader(new InputStreamReader(inputStream));
+			String[] line;
+            while ((line = reader.readNext()) != null) {
+            	JSONObject jsonObject = new JSONObject();
+            	jsonObject.put("userName", "" + line[1] + "");
+            	jsonObject.put("adminAcctType", "" + line[0] + "");
+            	jsonObject.put("adminAcctTypeName", "" + line[2] + "");
+            	jsonObject.put("unitNumber", "" + line[3] + "");  			
+    			jsonFileArray.put(jsonObject);
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		inputStream.close();
+		
+		Client client = Client.create();
+		// add basic authentication
+		client.addFilter(new HTTPBasicAuthFilter(REST_API_USER, REST_API_PASSWORD));
+		try {
+			JSONArray jsonUpdateArray = new JSONArray();
+			String existingUnitAdminTypes = getExistingUnitAdminTypes();
+			JSONArray unitAdminTypeJsonArray = new JSONArray(existingUnitAdminTypes);
+			for (int i = 0; i < jsonFileArray.length(); ++i) {
+				JSONObject jsonObj = jsonFileArray.getJSONObject(i);
+				String userId = getKualiUserId(jsonObj.getString("userName"));
+				String unitNumber = jsonObj.getString("unitNumber");
+				boolean unitFound = DbUtils.checkUnit(unitNumber, server);
+				if (userId == null) {
+					System.out.println("  ** User " + jsonObj.getString("userName") + " not found, or multiple records were returned for that user. **");
+				} else if (!unitFound) {
+					System.out.println("  ** Unit not found: " + unitNumber + " **");
+				} else {
+				//	System.out.println("User ID for user " + jsonObj.getString("userName") + " is " + userId);					
+					String adminTypeCode = null;
+					boolean found = false;
+					if (!existingUnitAdminTypes.equals(null)) {			
+						for (int ii = 0; ii < unitAdminTypeJsonArray.length(); ++ii) {
+							JSONObject unitAdminType = unitAdminTypeJsonArray.getJSONObject(ii);
+							if (unitAdminType.getString("description").equals(jsonObj.getString("adminAcctTypeName")) && unitAdminType.getString("defaultGroupFlag").equals(jsonObj.getString("adminAcctType"))) {
+						//		System.out.println("Type code for " + jsonObj.getString("adminAcctTypeName") + " is " + unitAdminType.getString("code"));
+								adminTypeCode = unitAdminType.getString("code");
+								found = true;
+								break;
+							}
+						}
+					}
+					if (!found) {
+						System.out.println("   *** Admin Type Code NOT FOUND: " + (jsonObj.getString("userName")) + ", " + (jsonObj.getString("adminAcctTypeName")));
+					} else {
+						System.out.println("   User id: " + userId + ", Admin Type Code: " + adminTypeCode + ", Unit Number: " + unitNumber);		
+						String unitAdminKey = userId + ":" + adminTypeCode + ":" + unitNumber;
+						WebResource webResource = client.resource(UNIT_ADMINISTRATOR_API_URL + unitAdminKey);
+						ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
 						
-						jsonUpdateArray.put(updateObj);
+						if (response.getStatus() == 200) {
+							System.out.println("   Unit administrator already exists in Kuali: " + (jsonObj.getString("userName")) + ", " + (jsonObj.getString("adminAcctTypeName")) + ", " + unitNumber + ", " + unitAdminKey);
+						} else {
+							JSONObject updateObj = new JSONObject();
+							updateObj.put("personId", "" + userId + "");
+							updateObj.put("unitAdministratorTypeCode", "" + adminTypeCode + "");
+							updateObj.put("unitNumber", "" + unitNumber + "");
+				//			System.out.println("Update Object: " + updateObj);
+							jsonUpdateArray.put(updateObj);
+						}
 					}
 				}
 			}
 			
-			System.out.println("JSON Update Array: " + jsonUpdateArray.toString());
-			Client client = Client.create();
-			// add basic authentication
-			client.addFilter(new HTTPBasicAuthFilter(REST_API_USER, REST_API_PASSWORD));		
-			WebResource webResource = client.resource(UNIT_ADMINISTRATOR_API_URL);
-			ClientResponse response = webResource.type("application/json").post(ClientResponse.class, jsonUpdateArray.toString());
-			// Response code 201 for success
-			if (response.getStatus() != 201) {
-				System.out.println("ERROR reponse code: " + response.getStatus());
+			if (jsonUpdateArray.length() > 0) {
+				System.out.println("JSON Update Array: " + jsonUpdateArray.toString());	
+				
+				WebResource webResource = client.resource(UNIT_ADMINISTRATOR_API_URL);
+				ClientResponse response = webResource.type("application/json").post(ClientResponse.class, jsonUpdateArray.toString());
+				// Response code 201 for success
+				if (response.getStatus() != 201) {
+					System.out.println("ERROR reponse code: " + response.getStatus());
+				} else {
+					System.out.println("   * Successful insert of unit administrators *");
+				}
+				
+			} else {
+				System.out.println("   * No unit administrators need to be inserted *");
+			//	System.out.println("     ***** JSON Update Array empty *****");
 			}
-			
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			client.destroy();
 		}
-		
 		
 		System.out.println("***************** END assignProjectAccountants() *****************\n");
 	}
 	
 	public static String getExistingUnitAdminTypes() throws IOException {
-		System.out.println("***************** getExistingUnitAdminTypes() *****************\n");
+	//	System.out.println("***************** getExistingUnitAdminTypes() *****************\n");
 		String output = null;
+		Client client = Client.create();
 		try {
-			Client client = Client.create();
+			
 			// add basic authentication
 			client.addFilter(new HTTPBasicAuthFilter(REST_API_USER, REST_API_PASSWORD));		
 			WebResource webResource = client.resource(UNIT_ADMIN_TYPE_API_URL);
 			ClientResponse getResponse = webResource.type("application/json").get(ClientResponse.class);
-			if (getResponse.getStatus() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : " + getResponse.getStatus());
-			} else {
+			
+			if (getResponse.getStatus() == 200) {
 				output = getResponse.getEntity(String.class);
-			}			
+			} else if (getResponse.getStatus() == 404) {
+				output = "notfound";
+			} else {
+				throw new RuntimeException("Failed : HTTP error code : " + getResponse.getStatus());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			client.destroy();
 		}
 		return output;
 	}
 	
 	public static void unitAdminTypeInsert() throws IOException {
-		System.out.println("***************** unitAdminTypeInsert() *****************\n");
+		System.out.println("***************** unitAdminTypeInsert() *****************");
 		InputStream inputStream = UpdateUnitAdminTypes.class.getClassLoader().getResourceAsStream("unit_admin_type_insert.json");
 		String jsonString = IOUtils.toString(inputStream, Charset.defaultCharset());
 		JSONArray jsonFileArray = new JSONArray(jsonString);
 		inputStream.close();
-		try {
-			Client client = Client.create();
-			// add basic authentication
-			client.addFilter(new HTTPBasicAuthFilter(REST_API_USER, REST_API_PASSWORD));		
-			WebResource webResource = client.resource(UNIT_ADMIN_TYPE_API_URL);
+		JSONArray unitAdminTypeUpdateArray = new JSONArray();		
+		int newCode = 0;
+		try {				
 			String existingUnitAdminTypes = getExistingUnitAdminTypes();
-			if (!existingUnitAdminTypes.equals(null)) {
+			if (!existingUnitAdminTypes.equals(null) && !existingUnitAdminTypes.equals("notfound")) {				
 				int maxCode = ApiUtils.getMaxCode(existingUnitAdminTypes, "code");
-				int newCode = maxCode + 1;
+				newCode = maxCode + 1;
+				
 				for (int i = 0; i < jsonFileArray.length(); ++i) {
-					jsonFileArray.getJSONObject(i).put("code", "" + newCode + "");
+					JSONObject jsonObj = jsonFileArray.getJSONObject(i);					
+					boolean found = false;
+					JSONArray existingUnitAdminTypeJsonArray = new JSONArray(existingUnitAdminTypes);					
+					for (int ii = 0; ii < existingUnitAdminTypeJsonArray.length(); ++ii) {
+						JSONObject existingUnitAdminType = existingUnitAdminTypeJsonArray.getJSONObject(ii);
+						if (existingUnitAdminType.getString("description").equals(jsonObj.getString("description")) && (existingUnitAdminType.getBoolean("multiplesFlag") == jsonObj.getBoolean("multiplesFlag")) && existingUnitAdminType.getString("defaultGroupFlag").equals(jsonObj.getString("defaultGroupFlag"))) {
+							found = true;
+							break;
+						}				
+					}
+					
+					if (!found) {
+						JSONObject updateObj = new JSONObject();
+						updateObj.put("description", "" + jsonObj.getString("description") + "");
+						updateObj.put("multiplesFlag", jsonObj.getBoolean("multiplesFlag"));
+						updateObj.put("defaultGroupFlag", "" + jsonObj.getString("defaultGroupFlag") + "");
+						updateObj.put("code", "" + newCode + "");
+						newCode++;
+					//	System.out.println("Update Object: " + updateObj);							
+						unitAdminTypeUpdateArray.put(updateObj);
+					} else {
+						System.out.println("   Unit Administrator Type already exists: " + jsonObj.getString("description"));
+					}					
+				}	
+			} else {		
+				newCode = 1;
+				for (int i = 0; i < jsonFileArray.length(); ++i) {
+					JSONObject jsonObj = jsonFileArray.getJSONObject(i);
+					JSONObject updateObj = new JSONObject();
+					updateObj.put("description", "" + jsonObj.getString("description") + "");
+					updateObj.put("multiplesFlag", jsonObj.getBoolean("multiplesFlag"));
+					updateObj.put("defaultGroupFlag", "" + jsonObj.getString("defaultGroupFlag") + "");
+					updateObj.put("code", "" + newCode + "");
 					newCode++;
+				//	System.out.println("Update Object: " + updateObj);							
+					unitAdminTypeUpdateArray.put(updateObj);
 				}
-				ClientResponse response = webResource.type("application/json").post(ClientResponse.class, jsonFileArray.toString());
+			}
+			if (unitAdminTypeUpdateArray.length() > 0) {
+		//		System.out.println("Post to API...");
+				System.out.println("JSON Update Array: " + unitAdminTypeUpdateArray.toString());	
+				Client client = Client.create();
+				// add basic authentication
+				client.addFilter(new HTTPBasicAuthFilter(REST_API_USER, REST_API_PASSWORD));		
+				WebResource webResource = client.resource(UNIT_ADMIN_TYPE_API_URL);
+				ClientResponse response = webResource.type("application/json").post(ClientResponse.class, unitAdminTypeUpdateArray.toString());
 				// Response code 201 for success
 				if (response.getStatus() != 201) {
-					System.out.println("ERROR reponse code: " + response.getStatus());
+					System.out.println("ERROR response code: " + response.getStatus());
+				} else {
+					System.out.println("   * Successful insert of unit administrator types *");
 				}
+			} else {
+				System.out.println("   * No unit administrator types need to be inserted *");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -188,9 +356,9 @@ public class UpdateUnitAdminTypes {
 	}
 	
 	public static void unitAdminTypeDelete() throws IOException {
-		System.out.println("***************** unitAdminTypeDelete() *****************\n");
+		System.out.println("***************** unitAdminTypeDelete() *****************");
 		String existingUnitAdminTypes = getExistingUnitAdminTypes();
-		if (!existingUnitAdminTypes.equals(null)) {
+		if (!existingUnitAdminTypes.equals(null) && !existingUnitAdminTypes.equals("notfound")) {
 			try {		
 				Client client = Client.create();
 				// add basic authentication
--- ucarenvsetup/src/main/java/edu/ucar/fanda/util/DbUtils.java
+++ ucarenvsetup/src/main/java/edu/ucar/fanda/util/DbUtils.java
@@ -8,16 +8,25 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DbUtils {
 	
-	public static Connection getConnection() {
+	public static Connection getConnection(String serverName) {
 		Connection conn = null;
+		String server = serverName;
+		int dbPort = 3306;
+		if (server.startsWith("kcnonprod")) {
+			String serverNumber = server.substring(server.length() - 1);
+			dbPort = Integer.parseInt(serverNumber + Integer.toString(dbPort));
+		}
+		
+		if (server != "localhost") {
+			server = server + ".fanda.ucar.edu";
+		}
+		
 		try {
 			MysqlDataSource dataSource = new MysqlDataSource();
 			dataSource.setUser("coeus");
 			dataSource.setPassword("kuali");
-	//		dataSource.setServerName("localhost");
-	//		dataSource.setServerName("kuali-eval.fanda.ucar.edu");
-			dataSource.setServerName("kcnonprod2.fanda.ucar.edu");
-			dataSource.setPort(23306);
+			dataSource.setServerName(server);
+			dataSource.setPort(dbPort);
 			dataSource.setDatabaseName("coeus");			
 			conn = dataSource.getConnection();
 		} catch (Exception e) {
@@ -26,12 +35,12 @@ public class DbUtils {
 		return conn;
 	}
 	
-	public static String getUserId(String userName) throws SQLException {
-		System.out.println("     ********************* getUserId() ********************\n");
+	public static String getUserId(String userName, String serverName) throws SQLException {
+//		System.out.println("     ********************* getUserId() ********************\n");
 		String userId = null;
 		Connection conn = null;
 		try {
-			conn = getConnection();
+			conn = getConnection(serverName);
 			if (conn != null) {
 				String sql = "SELECT prncpl_id FROM krim_prncpl_t WHERE prncpl_nm = ?";
 				PreparedStatement stmt = conn.prepareStatement(sql);
@@ -53,7 +62,31 @@ public class DbUtils {
 				conn.close();
 			}
 		}
-		System.out.println("     ********************* END getUserId() ********************\n");
+//		System.out.println("     ********************* END getUserId() ********************\n");
 		return userId;
+	}
+	
+	public static boolean checkUnit(String unit, String serverName) throws SQLException {
+		boolean unitFound = false;
+		Connection conn = null;
+		try {
+			conn = getConnection(serverName);
+			if (conn != null) {
+				String sql = "SELECT UNIT_NUMBER FROM unit WHERE unit_number = ?";
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				stmt.setString(1, unit);
+				ResultSet rs = stmt.executeQuery();
+				if (rs.next()) {
+					unitFound = true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		return unitFound;
 	}
 }
